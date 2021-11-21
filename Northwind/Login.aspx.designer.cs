5f1e4a6a63cdd3b1a0df6c3fca2303c146cd28c2
padischer <sven.walser26@gmail.com>
2021-11-21 16:52:39 +0100

    made css content for title and footer in masterpage

--- Northwind/Login.aspx.designer.cs
+++ Northwind/Login.aspx.designer.cs
@@ -33,15 +33,6 @@ namespace NewsletterSubscription
         protected global::System.Web.UI.WebControls.TextBox txtFirstName;
 
         /// <summary>
-        /// requiredFieldValidator1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator requiredFieldValidator1;
-
-        /// <summary>
         /// txtLastName control.
         /// </summary>
         /// <remarks>
